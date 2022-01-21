1e3d641c4c76a5b3cf9dd9238ee93a457a37fe08
Pu Wang <nicolas.weeks@gmail.com>
2022-01-21 13:49:32 +0800

    cargo build failure

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![deny(warnings)]
 #![allow(non_snake_case, non_camel_case_types, non_upper_case_globals)]
+#![allow(deref_nullptr)] // TODO(fxbug.dev/74605): Remove once bindgen is fixed.
 
 pub mod verbs;
 pub use verbs::*;
